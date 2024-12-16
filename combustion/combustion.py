c8e2dd812fd46e21ffc253279d4cdef4d932a100
[Ines Tang Yick] <[inestangyick@hotmail.com]>
2024-12-16 09:46:26 +0100

    comb
    Co-authored-by: jdurand-n7 <jdurand-n7@users.noreply.github.com>

--- combustion/combustion.py
+++ combustion/combustion.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # variables temps
-dt = 0.001
+dt = 0.00001
 tf = 10
 t = 0
 
@@ -13,46 +13,77 @@ t = 0
 P0 = 101325         # Pa 
 R0 = 0.001          # rayon initiale de la boule allumée
 T0 = 298.15         # température initiale des gaz frais
-R_p = 0.08          # à vérifier avec l'excel (rayon piston)
+R_p = 0.05          # rayon du piston
 V = 4*np.pi*R_p**3/3  # volume du piston
 
-rho_u = 1.2         # à calculer avec l'excel
-rho_b = 2.2         # à vérifier sur l'excel
-gamma = 1.3         # à vérifier sur l'excel
-YF0 = 0.01          # à calculer sur l'excel    
-
-T_gf_t = T0
-T_gb_t = 2888.56    # à corriger avec la température du cerfacs
-r_t = R0            # mm 
-P_t = P0
-QF = 1e6             # à calculer 
-
+rho_u = 1.374       
+rho_b = 1.7803e+01         
+gamma = 1.2806
+YF0 = 0.0625         
 
 # Expresion de sL en fonction de phi et coefficients
 phi = 1
 alpha = 2.18 - 0.8*(phi-1)
-beta = -0.16 + 0.22(phi-1)
-B_M = 26.3                   # cm/s
-B_phi = -84.7                # cm/s
+beta = -0.16 + 0.22*(phi-1)
+B_M = 26.3                      # cm/s
+B_phi = -84.7                    # cm/s
 phi_M = 1.13
-sL0 = B_M + B_phi*(phi-phi_M)**2 #cm/s
+sL0 = B_M + B_phi*(phi-phi_M)**2    #cm/s
+print("alpha = ", alpha)
+print("beta = ", beta)
+print("sL0 = ", sL0)
+
+# Nouvelle corrélation pour sL0
+A = 55.42
+B = -2.22e-14
+C = -171.9
+D = 74.61
+E = 153.7
+alpha_1 = 1.58
+alpha_2 = 0.04
+beta_1 = -0.203
+beta_2 = -9.44e-7
+sL0_ref = A + B*(phi - phi_M) + C*(phi - phi_M)**2 + D*(phi - phi_M)**3 + E*(phi - phi_M)**4
+alpha_s = alpha_1 + alpha_2*(phi - phi_M)
+beta_s = beta_1 + beta_2*(phi - phi_M)
+print("sL0_ref = ", sL0_ref)    
+sL0_new = sL0_ref * (T0/298.15)**alpha_s * (P0/101325)**beta_s
+print("sL0_new = ", sL0_new)
+
+r_mel = 8.314 / 0.03041748131
+r_bru = 290.7628415
+rho_gf = 1.393919518
+rho_gb = 1.7803e1
+
+
+# Variables en t 
+sL_t = sL0
+T_gf_t = T0
+T_gb_t = 2839.52    # température de fin de combustion (calculée pour isochore)
+r_t = R0            # mm 
+P_t = P0            # Pa
+QF = 44.7e6         # J/kg QLHV pouvoir calorifique du carburant
+i=0
 
 # variables à stocker
-t_tot = [T0]
+t_tot = [0]
 P_tot = [P0]
 T_gf_tot = [T0]
-T_gb_tot = [2888.56]
+T_gb_tot = [2839.52]
 sL_tot = [sL0]
 r_tot = [R0]  
 
 
 while r_t < 0.9*R_p :
-    T_gb_dt = 2888.56 + 1.006*(T_gb_t - T0)     # à vérifier 
-    P_dt = P_t + dt * (gamma-1)/V * QF * (4*np.pi * r_t**2 * rho_u * sL_t*YF0)
-    r_dt = r_t + dt * (rho_u/rho_b) * sL0*(T_gf_t/T0)**alpha * (P_t/P0)**beta
-    sL_dt = sL0 + (T_gf_t/T0)**alpha * (P_t/P0)**beta
+    T_gb_dt = 2839.52 + 1091.5625/1423.522842 * (T_gf_t - T0)
+    P_dt = P_t + dt * (gamma-1)/V * QF * (4*np.pi * r_t**2 * rho_gf * sL_t*YF0)
+    r_dt = r_t + dt * (rho_gf/rho_b) * sL0*(T_gf_t/T0)**alpha * (P_t/P0)**beta
+    #r_dt = r_t + dt * (rho_gf/rho_b) * sL_t
+    sL_dt = sL0 * (T_gf_t/T0)**alpha * (P_t/P0)**beta
     T_gf_dt = T_gf_t * ((R_p ** 3 - r_t ** 3) / (R_p ** 3 - r_dt ** 3)) ** (gamma - 1)
-
+    rho_gf = P_dt / (r_mel * T_gf_dt)
+    rho_gb = P_dt / (r_bru * T_gb_dt)
+    i+=1
 
     # changement des valeurs de variables avant l'itération suivante
     P_t = P_dt
@@ -69,4 +100,45 @@ while r_t < 0.9*R_p :
     T_gb_tot.append(T_gb_t)
     sL_tot.append(sL_t)
     r_tot.append(r_t)
+    
+#print('temp gaz frais : ' + str(T_gf_tot))    
+print('temps de combustion : ' + str(t))
+print('itérations : ' + str(i))
+
+# Tracé des courbes
+plt.figure(1)
+plt.loglog(t_tot, P_tot)
+plt.xlabel('temps (s)')
+plt.ylabel('pression (Pa)')
+plt.grid()
+plt.title('pression en fonction du temps')
+plt.savefig('pression.png')
+
+plt.figure(2)
+plt.loglog(t_tot, T_gb_tot)
+plt.xlabel('temps (s)')
+plt.ylabel('température des gaz brûlés (K)')
+plt.title('température des gaz brûlés en fonction du temps')
+plt.grid()
+plt.savefig('temp_gb.png')
+
+plt.figure(3)
+plt.loglog(t_tot, T_gf_tot)
+plt.xlabel('temps (s)')
+plt.ylabel('température des gaz frais (K)')
+plt.title('température des gaz frais en fonction du temps')
+plt.grid()
+plt.savefig('temp_gf.png')
+
+plt.figure(4)
+plt.loglog(t_tot, r_tot)
+plt.xlabel('temps (s)')
+plt.ylabel('rayon de la boule (m)')
+plt.title('rayon de la boule en fonction du temps')
+plt.grid()
+plt.savefig('rayon.png')
+
+
+plt.show()
+
 
